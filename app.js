00a3020eb5101c265a5b149d247f7620200e93bc
Amit Tambulkar <amittambulkar104@gmail.com>
2024-02-16 21:32:29 +0530

    chore: add accounts

---
+++ app.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// Accounts
+const account1 = {
+  owner: "Kate Bishop",
+  movements: [2000, 5253, -1790, -500, 50, -158, 240, 587],
+  pin: 1111,
+  loans: [500, 150],
+};
+
+const account2 = {
+  owner: "Livia Bator",
+  movements: [-200, 657, 790, -654, 54, -5500, 358, 581],
+  pin: 2222,
+  loans: [4000],
+};
+
+const account3 = {
+  owner: "Randy Press",
+  movements: [2345, 637, -24, 6541, -564, -3577, -65, -254],
+  pin: 3333,
+  loans: [0],
+};
+
+const account4 = {
+  owner: "Ryan Workman",
+  movements: [-545, -867, 36657, 54, 574, -1357, 851, -78],
+  pin: 4444,
+  loans: [10000],
+};
+
+const accounts = [account1, account2, account3, account4];
